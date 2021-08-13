52a7278049dbc248aee036e6b0c3245f48e11d35
HuangLuGuang <835923123@qq.com>
2021-08-13 17:29:33 +0800

    feat: one_cummins页面鼠标悬浮变色

--- src/views/kpi/one_cummins.vue
+++ src/views/kpi/one_cummins.vue
@@ -1,36 +1,72 @@
 <template>
   <div class="flex flex-col items-center justify-center py-8 bg-white">
     <img
+      class="parent"
       src="../../assets/images/kpi/one_cummins.png"
       alt="planets"
       usemap="#plantmap"
       @click="doclick($event)"
     />
+    <!-- Deploy-to-Deploy -->
+    <a href="/#/comp/basic">
+      <img
+        class="opacity-0 relative right-15.25rem bottom-29.41rem hover:opacity-100"
+        src="../../assets/images/kpi/idea_to_deploy.png"
+      />
+    </a>
+    <!-- order-to-cash -->
+    <a href="/#/comp/basic">
+      <img
+        class="opacity-0 relative right-7.05rem bottom-48.08rem hover:opacity-100"
+        src="../../assets/images/kpi/order_to_cash.png"
+      />
+    </a>
+    <!-- Source-to-Pay -->
+    <a href="/#/comp/basic">
+      <img
+        class="opacity-0 relative left-6.3rem bottom-52.59rem hover:opacity-100"
+        src="../../assets/images/kpi/source_to_pay.png"
+      />
+    </a>
+    <!-- Plan-to-Build -->
+    <a href="/#/comp/basic">
+      <img
+        class="opacity-0 relative left-10.87rem bottom-48.05rem hover:opacity-100"
+        src="../../assets/images/kpi/plan_to_build.png"
+      />
+    </a>
+    <!-- Issue-to-Resolution -->
+    <a href="/#/comp/basic">
+      <img
+        class="opacity-0 relative left-2.47rem bottom-44.9rem hover:opacity-100"
+        src="../../assets/images/kpi/issue_to_resolution.png"
+      />
+    </a>
+    <!-- Record-to-Report -->
+    <a href="/#/comp/basic">
+      <img
+        class="opacity-0 relative right-11.05rem bottom-53.4rem hover:opacity-100"
+        src="../../assets/images/kpi/record_to_report.png"
+      />
+    </a>
     <map id="plantmap" name="plantmap">
-      <!-- Customer left -->
-      <area shape="rect" coords="237,252,428,361" href="/#/comp/basic" />
-
-      <area shape="rect" coords="584,221,633,328" href="/#/comp/basic" title="Deploy" />
-      <area shape="rect" coords="678,120,767,154" href="/#/comp/basic" title="Order" />
-      <area shape="rect" coords="864,115,921,220" href="/#/comp/basic" title="Source" />
-      <area shape="rect" coords="933,280,981,393" href="/#/comp/basic" title="Plan" />
-      <area shape="rect" coords="792,449,914,476" href="/#/comp/basic" title="Issue" />
-      <area shape="rect" coords="632,370,716,499" href="/#/comp/basic" title="Record" />
-
       <!-- Ethics, Compliance, Environment and Legal Managment -->
-      <area shape="rect" coords="510,190,548,361" href="/#/comp/basic" />
+      <area shape="rect" coords="338,192,383,503" href="/#/comp/basic" />
 
       <!-- Pople, Resource & Asset Managment -->
-      <area shape="rect" coords="1020,198,1050,408" href="/#/comp/basic" />
+      <area shape="rect" coords="919,211,970,553" href="/#/comp/basic" />
 
       <!-- Functional & Intergrated Leadership -->
-      <area shape="rect" coords="677,14,920,35" href="/#/comp/basic" />
+      <area shape="rect" coords="519,5,832,49" href="/#/comp/basic" />
 
       <!-- Mission, Vision and Value -->
-      <area shape="rect" coords="688,574,883,592" href="/#/comp/basic" />
+      <area shape="rect" coords="527,667,761,712" href="/#/comp/basic" />
+
+      <!-- Customer left -->
+      <area shape="rect" coords="4,294,230,425" href="/#/comp/basic" />
 
       <!-- Customer right -->
-      <area shape="rect" coords="1177,252,1374,361" href="/#/comp/basic" />
+      <area shape="rect" coords="1112,298,1342,428" href="/#/comp/basic" />
     </map>
   </div>
 </template>
@@ -43,14 +79,15 @@
     setup() {
       function doclick(event: any) {
         console.warn(event.offsetX, event.offsetY);
+        alert(`${event.offsetX},${event.offsetY}`);
       }
       return { doclick };
     },
   });
 </script>
 <style lang="less" scoped>
-  img {
-    width: 1620px;
-    min-width: 1620px;
+  .parent {
+    width: 1376px;
+    min-width: 1376px;
   }
 </style>
